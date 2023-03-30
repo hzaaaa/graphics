ec3e2eaec79cb6ada78e5f06f5e2bea081d23800
Hzaaaa <2945109196@qq.com>
2023-03-31 03:42:28 +0800

    ref point

---
+++ b.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int num = 5;
+void func()
+{
+    // printf("fun in a.c");
+    cout<<"func"<<endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 using namespace std;
 void testPointAndReference();
+void testExtern()
+{
+    extern int num;
+    cout << num << endl;
+    extern void func();
+    func();
+}
+void testTypedef()
+{
+    typedef double d2;
+    d2 b = 1.23;
+    cout<<b<<endl;
+    using d3 = double;
+    d3 c =2.34;
+    cout<<c<<endl;
+}
 int main()
 {
     cout << "test" << endl;
-    testPointAndReference();
+testTypedef();
     return 0;
 }
-
-
 
 void testPointAndReference()
 {
