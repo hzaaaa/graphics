7b303021a390bad103355b55068278b17dd45cfe
Hzaaaa <2945109196@qq.com>
2023-04-20 12:12:09 +0800

    1

--- backup/study/main.cpp
+++ backup/study/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <deque>
+#include <math.h>
+#include <tuple>
+
+#include <limits.h>
 #include "a.hpp"
 using namespace std;
 
@@ -57,81 +62,84 @@ int ifunc(int a)
     return a;
 }
 
-f* returnRef( )
+f *returnRef()
 {
     // f* f1 = returnRef();
     // (*f1)(1);
     return ifunc;
 }
 
-vector<string> vs2vS(vector<string> vs){
+vector<string> vs2vS(vector<string> vs)
+{
     // vector<string> vs ={"qwe","sdf"};
     // vs = vs2vS(vs);
     // for(string &s : vs){
     //    cout<<s<<endl;
     // }
-    for(string &s : vs){
-        for(char &c :s){
-            c=toupper(c);
+    for (string &s : vs)
+    {
+        for (char &c : s)
+        {
+            c = toupper(c);
         }
     }
     return vs;
 }
-void threeMethod(){
-    vector<int> vi1 ={42,42,42};
-    vector<int> vi2(3,42);
-    for(int i :vi1){
-        cout<<i<<endl;
+void threeMethod()
+{
+    vector<int> vi1 = {42, 42, 42};
+    vector<int> vi2(3, 42);
+    for (int i : vi1)
+    {
+        cout << i << endl;
     }
-    for(int i :vi2){
-        cout<<i<<endl;
+    for (int i : vi2)
+    {
+        cout << i << endl;
     }
-
 }
-void coutVectorHeadTail(){
-    vector<int>vi={1,2,3,4,5,6,7,8};
+void coutVectorHeadTail()
+{
+    vector<int> vi = {1, 2, 3, 4, 5, 6, 7, 8};
     auto h = vi.begin();
     auto p1 = h;
 
     auto t = vi.end();
-    auto p2 = t-1;
+    auto p2 = t - 1;
 
-    while (p1!=t)
+    while (p1 != t)
     {
-        int sum = *p1+*p2;
-        cout<<"1:"<<*p1<<endl;
-        cout<<sum<<endl;
+        int sum = *p1 + *p2;
+        cout << "1:" << *p1 << endl;
+        cout << sum << endl;
         p1++;
         p2--;
-        
     }
-    
-    
 }
 
-void testError(){
-    int n1,n2;
-    cin>>n1>>n2;
-    
+void testError()
+{
+    int n1, n2;
+    cin >> n1 >> n2;
 }
 // AC o2 =  AC();
 //     cout<<o2.t<<endl;
-class AC{
-    public:
-        int t=5;
+class AC
+{
+public:
+    int t = 5;
 };
 
-int testf1(int a){
+int testf1(int a)
+{
     // cout<<testf1(1)<<endl;
     // cout<<testf1(1)<<endl;
     // cout<<testf1(1)<<endl;
     // cout<<testf1(1)<<endl;
     // cout<<testf1(1)<<endl;
     static int c = 0;
-    
+
     return c++;
-    
-    
 }
 void sTox(string &s)
 {
@@ -147,30 +155,36 @@ void sTox(string &s)
 int ah = 123;
 // af();
 // bf();
-void swapValue(int &a,int &b){
+void swapValue(int &a, int &b)
+{
     // int a = 1;
     // int b=2;
     // swapValue(a,b);
     // cout<<"a:"<<a<<endl;
     // cout<<"b:"<<b<<endl;
-    int c =a;
-    a=b;
-    b=c;
+    int c = a;
+    a = b;
+    b = c;
 }
-int returnBig(int&a,int*b){
+int returnBig(int &a, int *b)
+{
     // int a=1;
     // int b=2;
 
     // int c =returnBig(a,&b);
     // cout<<"c:"<<c<<endl;
 
-    if(a>*b){
+    if (a > *b)
+    {
         return a;
-    }else{
+    }
+    else
+    {
         return *b;
     }
 }
-void swapPoint(int* (&p1) ,int* (&p2)  ){
+void swapPoint(int *(&p1), int *(&p2))
+{
     // int a = 1;
     // int b = 2;
     // int*p1 = &a;
@@ -181,44 +195,167 @@ void swapPoint(int* (&p1) ,int* (&p2)  ){
     // cout<<"*p1:"<<*p1<<endl;
     // cout<<"*p2:"<<*p2<<endl;
 
-    int* p3 =p1;
-    p1=p2;
-    p2=p3;
+    int *p3 = p1;
+    p1 = p2;
+    p2 = p3;
 }
-void test6_2_6_27(initializer_list<int> il){
+void test6_2_6_27(initializer_list<int> il)
+{
     // test6_2_6_27({1,2,3});
     // test6_2_6_27({1});
     // test6_2_6_27({1,3,4,5,6,7});
 
     int sum = 0;
-    for(int i :il){
-        sum+=i;
+    for (int i : il)
+    {
+        sum += i;
     }
-    cout<<sum<<endl;
+    cout << sum << endl;
 }
-void recursion(std::vector<int>::iterator start, std::vector<int>::iterator end){
+void recursion(std::vector<int>::iterator start, std::vector<int>::iterator end)
+{
     // vector<int> v ={1,2,3,4,5,6,7};
     // recursion(v.begin(),v.end()-1);
 
-
-    if(start==end){
-        cout<<(*start)<<endl;
+    if (start == end)
+    {
+        cout << (*start) << endl;
         return;
-    }else if(start==(end+1)){
+    }
+    else if (start == (end + 1))
+    {
         return;
-    }else{
-        cout<<*start<<"   "<<*end<<endl;
+    }
+    else
+    {
+        cout << *start << "   " << *end << endl;
         start++;
         end--;
-        recursion(start,end);
+        recursion(start, end);
     }
-
 }
-int main(int argc,char *sa[])//sa实际上是指针 无大小
+void testCopy()
 {
-    //透明
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = v1;
+    cout << v1[0] << endl;
+    v2[0] = 4;
+    cout << v1[0] << endl;
+}
+class Solution
+{
+public:
+    int maxSumAfterPartitioning(vector<int> &arr, int k)
+    {
+        
+    }
+#pragma region
+    int maxSumAfterPartitioningError(vector<int> &arr, int k)
+    {
+        int l = arr.size();
+
+        vector<int> resultA(l);
+        deque<tuple<int, int>> reduceList; // value index
+
+        int maxV = INT_MIN;
+        int p = -1;
+        for (int i = 0; i < k && i < l; i++)
+        {
+            int value = arr[i];
+            if (value > maxV)
+            {
+                maxV = value;
+                p = i;
+            }
+
+            if (reduceList.empty())
+            { // null
+                tuple<int, int> t{value, i};
+                reduceList.push_back(t);
+            }
+            else
+            {
+                while ((!reduceList.empty()) && value > get<0>(reduceList.back()))
+                {
+                    reduceList.pop_back();
+                }
+                tuple<int, int> t{value, i};
+                reduceList.push_back(t);
+            }
+
+            resultA[i] = maxV * (i + 1);
+        }
+
+        for (int i = k; i < l; i++)
+        {
+            int value = arr[i];
+            if (reduceList.empty())
+            { // null
+                tuple<int, int> t{value, i};
+                reduceList.push_back(t);
+            }
+            else
+            {
+                while ((!reduceList.empty()) && value > get<0>(reduceList.back()))
+                {
+                    reduceList.pop_back();
+                }
+                tuple<int, int> t{value, i};
+                reduceList.push_back(t);
+            }
+            auto maxTuple = reduceList.front();
+            while ((i - get<1>(maxTuple)) >= k)
+            {
+                reduceList.pop_front();
+                maxTuple = reduceList.front();
+            }
+
+            // while(reduceList)
+
+            int a = resultA[i - 1] + value; // k=1
+
+            int b = k * get<0>(maxTuple) + resultA[i - k]; // k=k
+            resultA[i] = max(a, b);
+
+            for (auto item : reduceList)
+            {
+                int tempV = (i - get<1>(item) + 1) * get<0>(item) + resultA[get<1>(item) - 1]; // 获取队列 大值
+                resultA[i] = max(tempV, resultA[i]);
+                //  tempV =  (i-get<1>(item)+1) * get<0>(item) + resultA[get<1>(item)]; //获取队列 大值
+                //  resultA[i] = max(tempV, resultA[i]);
+            }
+
+            // cout<<"a:"<<a<<"  b:"<<b<<endl;
+            // cout<<"i:"<<i<<"  value:"<<value<<endl;
+        }
+        // cout<<resultA.<<endl;
+        for (int r : resultA)
+        {
+            cout << r << " ";
+        }
+        cout << endl;
+        return resultA[l - 1];
+    }
+#pragma endregion
+};
+int main(int argc, char *sa[]) // sa实际上是指针 无大小
+{
+    Solution s;
+    // vector<int> v{1,4,1,5,7,3,6,1,9,9,3};
+    // int k=4;
+    vector<int> v{581920, 200100, 526972, 677934, 989025, 294832, 581857, 663919, 961334, 1881, 859626, 251114, 361905, 867006, 86001, 836946, 894279, 859796, 750025, 898166, 674980, 939280, 458563, 117387, 403337, 961067, 589597, 73584, 178968, 955900, 755634, 609754, 97612, 19699, 843950, 907919, 883083, 522938, 670930, 671679, 390248, 704834, 188158, 226340, 80496, 701795, 867627, 280988, 465918, 748627, 321025, 564279, 79716, 786173};
+    int k = 23;
+    // vector<int> v{1,15,7,9,2,5,10};
+    // int k=3;
+    for (int r : v)
+    {
+        // cout << r << " ";
+    }
+    cout << endl;
+    int result = s.maxSumAfterPartitioning(v, k);
+    cout << result << endl;
     //
-    
+    //
 }
 // // for(char *s : sa){//error
 //     //     cout<<s<<endl;
@@ -229,4 +366,3 @@ int main(int argc,char *sa[])//sa实际上是指针 无大小
 //         s+=sa[i];
 //     }
 //     cout<<s<<endl;
-
